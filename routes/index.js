12346ef0a8dc1360d033cbc58dc07179a2f1bcfb
juanbig <juannodecoorp@gmail.com>
2025-01-26 04:23:03 -0500

    Updated routes/index.js +++

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Us' });
+});
+
+router.get('/data', async function(req, res, next) {
+  try {
+    const data = await fetchDataFromDatabase(); // Replace with actual data fetching logic
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
