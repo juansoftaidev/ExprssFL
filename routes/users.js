58ebfaee9f1aebd8772af5ffae7ea50f481534c5
juanbig <juannodecoorp@gmail.com>
2025-01-26 04:25:58 -0500

    Updated routes/users.js +++

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/:id', function(req, res, next) {
+  const userId = req.params.id;
+  // Replace with actual logic to fetch user data by ID
+  res.json({ id: userId, name: 'User  Name' });
 });
 
 module.exports = router;
